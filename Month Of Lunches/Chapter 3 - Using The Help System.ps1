e494ff7f04518909133f97f22a565d598caad541
Dennis Ofoborh <dofoborh@hotmail.co.uk>
2014-08-13 16:54:54 +0100

    lab notes for chapter 3

--- Month Of Lunches/Chapter 3 - Using The Help System.ps1
+++ Month Of Lunches/Chapter 3 - Using The Help System.ps1
@@ -3,10 +3,32 @@
 #First Lab Answers:
 
 
-1. 
-2. convertTo-Html
-3. out-file
-4. 
-5. write-eventlog	
-6. 
+#1. 
+#2. convertTo-Html
+#3. out-file
+#4. 
+#5. write-eventlog	
+#6.  
+#7. Start-Transcript stop-transcript
+#8. Get-EventLog Security -Newest 100
+#9. get-service
+#10 get-process 
 
+#11 get-process | out-file c:\test\test.txt -width 20
+
+#12 get-process | out-file c:\test\test.txt -noclobber
+
+#13
+#14
+#15
+#16
+#17
+#18
+#19
+#20
+
+
+
+
+
+help Out-File -full
