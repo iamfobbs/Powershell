c01401952d6cb6ce5e2ed0a21647de4e47a6a7c4
Dennis <dofoborh@hotmail.co.uk>
2014-08-19 21:56:43 +0100

    Chapter 5 attempted

--- Month Of Lunches/Chapter 5 - Working With Providers.ps1
+++ Month Of Lunches/Chapter 5 - Working With Providers.ps1
@@ -23,26 +23,41 @@ Set-ItemProperty -Path advanced DontPrettyPath -Value 1
 Get-ChildItem
 
 
+#2 Create new item
 
-#2
-#3
+New-Item C:\Temp\test.txt -ItemType directory
+Rename-Item C:\Temp\test.txt -NewName test
+
+#note that "item" is generic for powershell therefore you need to specify
+# .. the type of file/item you would like to create.
+
+#Answer!
+New-Item C:\Temp\test\testing.txt -ItemType file
+
+#3 Setting the name of the file to something else.
+
+cd .\test
+dir
+
+help Set-Item -Examples
+
+
+Set-Item -Path C:\Temp\test\testing.txt -Value TESTER.txt
+
+Answer: fails because it only changes the value of an item rather than the item itself
+
 #4
-#5
-#6
-#7
-#8
-#9
-#10
-#11
-#12
-#13
-#14
-#15
-#16
-#17
-#18
-#19
+-filter : filter by the specified value
+-include: include those with a particular name type/value/extension etc
+-exclude: excludes item specified / a particular type of file you do not want to see
 
+
+Get-ChildItem -Include *.txt
+Get-ChildItem -filter *.txt
+Get-ChildItem -exclude *.txt
+
+
+help Get-ChildItem -Examples
 
 
 
