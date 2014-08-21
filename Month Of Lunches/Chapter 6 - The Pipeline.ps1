8cc9f93b257c9a885545fa06e2ed0ae1f6552ac5
Dennis Ofoborh <dofoborh@hotmail.co.uk>
2014-08-21 16:54:15 +0100

    Ch6 ~ import & exporting

--- Month Of Lunches/Chapter 6 - The Pipeline.ps1
+++ Month Of Lunches/Chapter 6 - The Pipeline.ps1
@@ -1,6 +1,18 @@
 ﻿Chapter 6 - The Pipeline
 
 #1
+
+ps | Export-Csv -LiteralPath c:\temp\procs.csv
+
+ps | Export-Clixml -LiteralPath C:\Temp\procs.xml
+
+
+Import-Clixml -Path C:\Temp\procs.xml
+
+
+Get-Process | Export-Clixml C:\Temp\ref.xml
+
+
 #2
 #3
 #4
@@ -20,7 +32,7 @@
 #18
 #19
 
-
+dir 
 
 
 ANSWERS
